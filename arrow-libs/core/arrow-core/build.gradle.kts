f7ba51d5ec44fc499e90d1c4248c1859a8d52908
Youssef Shoaib <canonballt@gmail.com>
2023-02-26 04:53:39 +0000

    Add OptionBenchmark

--- arrow-libs/core/arrow-core/build.gradle.kts
+++ arrow-libs/core/arrow-core/build.gradle.kts
@@ -1,5 +1,6 @@
 @file:Suppress("DSL_SCOPE_VIOLATION")
 
+import kotlinx.benchmark.gradle.KotlinJvmBenchmarkTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -7,6 +8,8 @@ plugins {
   alias(libs.plugins.arrowGradleConfig.kotlin)
   alias(libs.plugins.arrowGradleConfig.publish)
   alias(libs.plugins.kotest.multiplatform)
+  id("org.jetbrains.kotlinx.benchmark") version "0.4.7"
+  id("org.jetbrains.kotlin.plugin.allopen") version "1.8.10"
 }
 
 apply(from = property("TEST_COVERAGE"))
@@ -23,12 +26,31 @@ if (enableCompatibilityMetadataVariant) {
 }
 
 kotlin {
+  targets {
+    jvm {
+      compilations {
+        val main by getting
+        val benchmarks by creating {
+          associateWith(main)
+        }
+
+        benchmark.targets.add(
+          KotlinJvmBenchmarkTarget(
+            benchmark,
+            benchmarks.defaultSourceSet.name,
+            benchmarks,
+          )
+        )
+      }
+    }
+  }
   sourceSets {
     commonMain {
       dependencies {
         api(projects.arrowAtomic)
         api(projects.arrowAnnotations)
         api(libs.kotlin.stdlibCommon)
+        implementation("org.jetbrains.kotlinx:kotlinx-benchmark-runtime:0.4.4")
       }
     }
     if (!enableCompatibilityMetadataVariant) {
@@ -58,10 +80,28 @@ kotlin {
         implementation(libs.kotlin.stdlibJS)
       }
     }
+
+    val jvmMain by getting
+    val jvmBenchmarks by getting {
+      dependsOn(jvmMain)
+      dependencies {
+        implementation("org.openjdk.jmh:jmh-core:1.21")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+      }
+    }
   }
 }
 
 // enables context receivers for Jvm Tests
 tasks.named<KotlinCompile>("compileTestKotlinJvm") {
   kotlinOptions.freeCompilerArgs += "-Xcontext-receivers"
+}
+
+// enables context receivers for Jvm Benchmarks
+tasks.named<KotlinCompile>("compileBenchmarksKotlinJvm") {
+  kotlinOptions.freeCompilerArgs += "-Xcontext-receivers"
+}
+
+allOpen {
+  annotation("org.openjdk.jmh.annotations.State")
 }
